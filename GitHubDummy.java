5fabd6e888e6d3c5d135066cd2f6907d092160b1
Nick <nicholas.henegar@gmail.com>
2016-09-12 17:12:30 -0700

    Add Spongebob to dummy

--- GitHubDummy.java
+++ GitHubDummy.java
@@ -4,7 +4,9 @@ public class GitHubDummy {
 	public static void main(String[] args) {
 		System.out.println("Hey, this is a dummy.");
 		System.out.println("I'm testing GitHub!");
-
+		for (int i = 0; i < 4; i++){
+			System.out.println("I'm ready!");
+		}
 	}
 
 }
